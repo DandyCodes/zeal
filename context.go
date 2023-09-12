32a8d6197cebcdc3ce104e919f6bdb03b562f3a5
John Barker <dandycodesdev@gmail.com>
2023-09-12 23:15:00 +0800

    fix: JSON status parameter name

--- context.go
+++ context.go
@@ -9,11 +9,11 @@ type Writer[ResponseType any] struct {
 	http.ResponseWriter
 }
 
-func (w *Writer[ResponseType]) JSON(data ResponseType, statuses ...int) {
+func (w *Writer[ResponseType]) JSON(data ResponseType, status ...int) {
 	w.Header().Add("Content-Type", "application/json")
-	if len(statuses) > 0 {
-		status := statuses[0]
-		w.WriteHeader(status)
+	if len(status) > 0 {
+		firstStatus := status[0]
+		w.WriteHeader(firstStatus)
 	}
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
